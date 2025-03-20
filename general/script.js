d31bd90a777a70d096cf411bc0a7a96fde05ab16
Shamoji <138852694+ShamojiSMM@users.noreply.github.com>
2025-03-20 23:49:43 +0900

    Update script.js

--- general/script.js
+++ general/script.js
@@ -15,7 +15,7 @@ function getElm(selector) {
 }
 
 function getElmAll(selector) {
-  return document.querySelectorAll(selector);
+  return Array.from(document.querySelectorAll(selector));
 }
 
 function addOption(select, text, value) {
@@ -39,3 +39,65 @@ function changeVisible(element, isVisible, isToParent = true) {
   if (isVisible) classList.remove("invisible");
   else classList.add("invisible");
 }
+
+const languages = {
+  "ja": "日本語",
+  "en": "English(beta)"
+};
+
+const selectLanguage = getElm("#selectLanguage");
+Object.keys(languages).forEach(key => {
+  addOption(selectLanguage, languages[key], key);
+});
+
+function parseCSVLine(line) {
+  const parts = [];
+
+  let current = "";
+  let inQuotes = false;
+
+  for (let i = 0, l = line.length; i < l; i ++) {
+    const char = line[i];
+    const nextChar = line[i + 1];
+
+    if (char == `"`) {
+      if (inQuotes && nextChar == `"`) {
+        current += `"`;
+        i ++;
+
+      } else inQuotes = !inQuotes;
+
+    } else if (char == `,` && !inQuotes) {
+      parts.push(current);
+      current = "";
+
+    } else current += char;
+  }
+
+  parts.push(current);
+  return parts;
+}
+
+async function changeLanguage() {
+  const lang = selectLanguage.value;
+  sessionStorage.lang = lang;
+
+  const res = await fetch(`langs/${lang}.csv`);
+  const text = await res.text();
+
+  text.trim().split(/\r?\n/).forEach(line => {
+    if (line[0] != "#" && line != "") {
+      const parts = parseCSVLine(line);
+
+      getElmAll(`[data-l="${parts[0]}"]`).forEach(elm => {
+        elm.textContent = parts[1];
+        if (parts[2] != "") elm.title = parts[2];
+      });
+    }
+  });
+}
+
+selectLanguage.value = sessionStorage?.lang || "ja";
+
+selectLanguage.addEventListener("change", changeLanguage);
+changeLanguage();
